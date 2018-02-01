76eb1f4c40610c96cbc2d3ca08b6098c50439fc1
crusty <crusty@crusty.com>
2018-02-01 16:46:29 -0700

    wip

--- config/council.php
+++ config/council.php
@@ -3,7 +3,8 @@
 return [
     'administrators' => [
         // Add the email addresses of users who should be administrators here.
-        'd@d.com'
+        'd@d.com',
+        'john@example.com'
     ],
     'reputation' => [
         'thread_published' => 10,
